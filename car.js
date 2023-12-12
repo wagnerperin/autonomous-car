53a01c6853f4590344ba37e50a01de6fbf038e6c
Wagner Perin <wagnerperin@gmail.com>
2023-12-12 00:59:14 -0300

    Adding controls

--- car.js
+++ car.js
@@ -4,8 +4,25 @@ class Car{
         this.y = y;
         this.width = width;
         this.height = height;
+
+        this.controls = new Controls();
     }
     
+    update(){
+        if(this.controls.forward){
+            this.y -= 2;
+        }
+        if(this.controls.reverse){
+            this.y += 2;
+        }
+        if(this.controls.left){
+            this.x -= 2;
+        }
+        if(this.controls.right){
+            this.x += 2;
+        }
+    }
+
     draw(ctx){
         ctx.beginPath();
         ctx.rect(
---
+++ controls.js
@@ -0,0 +1,50 @@
+class Controls{
+    constructor(){
+        this.forward = false;
+        this.reverse = false;
+        this.left = false;
+        this.right = false;
+
+        this.#addKeyListeners();
+    }
+    
+    #addKeyListeners(){
+        document.addEventListener('keydown', (event) => {
+            switch(event.key){
+                case 'ArrowUp':
+                    this.forward = true;
+                    break;
+                case 'ArrowDown':
+                    this.reverse = true;
+                    break;
+                case 'ArrowLeft':
+                    this.left = true;
+                    break;
+                case 'ArrowRight':
+                    this.right = true;
+                    break;
+            }
+
+            console.table(this);
+        });
+
+        document.addEventListener('keyup', (event) => {
+            switch(event.key){
+                case 'ArrowUp':
+                    this.forward = false;
+                    break;
+                case 'ArrowDown':
+                    this.reverse = false;
+                    break;
+                case 'ArrowLeft':
+                    this.left = false;
+                    break;
+                case 'ArrowRight':
+                    this.right = false;
+                    break;
+            }
+
+            console.table(this);
+        });
+    }    
+}
