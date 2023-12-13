88c1bf47b876b847581963b1539a05f97a432d40
Wagner de Andrade Perin <wagnerperin@gmail.com>
2023-12-13 12:52:37 -0300

    Adding traffic on the road.

--- car.js
+++ car.js
@@ -1,5 +1,5 @@
 class Car{
-    constructor(x,y,width,height){
+    constructor(x,y,width,height, controlType, maxSpeed=3){
         this.x = x;
         this.y = y;
         this.width = width;
@@ -7,13 +7,15 @@ class Car{
 
         this.speed = 0;
         this.acceleration = 0.2;
-        this.maxSpeed = 3;
+        this.maxSpeed = maxSpeed;
         this.friction = 0.05;
         this.angle = 0;
         this.damaged = false;
 
-        this.sensor = new Sensor(this);
-        this.controls = new Controls();
+        if(controlType == 'KEYS'){
+            this.sensor = new Sensor(this);
+        }
+        this.controls = new Controls(controlType);
     }
     
     #move(){
@@ -57,20 +59,23 @@ class Car{
         this.y -= this.speed * Math.cos(this.angle);
     }
 
-    update(roadBorders){
+    update(roadBorders, traffic){
         if(!this.damaged){
             this.#move();
             this.polygon = this.#createPolygon();
-            this.damaged = this.#assessDamage(roadBorders);
+            this.damaged = this.#assessDamage(roadBorders, traffic);
         }
-        this.sensor.update(roadBorders);
+        if(this.sensor) this.sensor.update(roadBorders, traffic);
     }
 
-    #assessDamage(roadBorders){
+    #assessDamage(roadBorders, traffic){
         let hasDamage = false;
 
         roadBorders.forEach((border) => {
             if(hasPolysIntersect(this.polygon, border)) hasDamage = true;
+        });
+        traffic.forEach((car) => {
+            if(hasPolysIntersect(this.polygon, car.polygon)) hasDamage = true;
         });
 
         return hasDamage;
@@ -101,11 +106,11 @@ class Car{
         return points;
     }
 
-    draw(ctx){
+    draw(ctx, color){
         if(this.damaged){
-            ctx.fillStyle = 'red';
+            ctx.fillStyle = 'gray';
         }else{
-            ctx.fillStyle = 'black';
+            ctx.fillStyle = color;
         }
         ctx.beginPath();
         ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
@@ -114,6 +119,6 @@ class Car{
         }
         ctx.fill();
 
-        this.sensor.draw(ctx);
+        if(this.sensor) this.sensor.draw(ctx);
     }
 }
--- sensor.js
+++ sensor.js
@@ -9,15 +9,15 @@ class Sensor{
         this.readings = [];
     }
 
-    update(roadBorders){
+    update(roadBorders, traffic){
         this.#castRays();
         this.readings = [];
         this.rays.forEach((ray) => {
-            this.readings.push(this.#getReading(ray, roadBorders));
+            this.readings.push(this.#getReading(ray, roadBorders, traffic));
         });
     }
 
-    #getReading(ray, roadBorders){
+    #getReading(ray, roadBorders, traffic){
         let touches = [];
         roadBorders.forEach((border) => {
             const intersection = getIntersection(ray[0], ray[1], border[0], border[1]);
@@ -25,6 +25,16 @@ class Sensor{
                 touches.push(intersection);
             }
         });
+
+        for(let i = 0; i < traffic.length; i++){
+            const poly = traffic[i].polygon;
+            for(let j = 0; j < poly.length; j++){
+                const intersection = getIntersection(ray[0], ray[1], poly[j], poly[(j+1)%poly.length]);
+                if(intersection){
+                    touches.push(intersection);
+                }
+            }
+        }
 
         if(touches.length == 0){
             return null;
